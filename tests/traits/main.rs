ba83c839b61969264cb544538015b33d7f5ec9b0
Oleg Ianchenko <olegian@cs.washington.edu>
2026-04-27 19:35:53 -0700

    make trait test more interesting

--- tests/traits/main.rs
+++ tests/traits/main.rs
@@ -13,6 +13,21 @@ where
     }
 }
 
+enum MyEnum<T> { V1(T), V2(T, T) }
+impl std::ops::Add for MyEnum<u32> {
+    type Output = MyEnum<u32>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (MyEnum::V1(a), MyEnum::V1(c)) => MyEnum::V1(a + c),
+            (MyEnum::V1(a), MyEnum::V2(c, d)) => MyEnum::V2(a + c, d),
+            (MyEnum::V2(a, b), MyEnum::V1(c)) => MyEnum::V2(a + c, b),
+            (MyEnum::V2(a, b), MyEnum::V2(c, d)) => MyEnum::V2(a + c, b + d),
+        }
+    }
+}
+
+
 fn main() { 
     foo(MyStruct(10, 20), MyStruct(30, 40));
 }
